434b6567001eb59a33f8b8561bf19569a554eae2
Tonni Tølbøll Lund Aagesen <tonni@toelboell-lund.dk>
2012-12-24 15:59:15 +0100

    Heroku friendly way of establishing database connections with ActiveRecord

--- config/environment.rb
+++ config/environment.rb
@@ -11,11 +11,12 @@ Bundler.setup(:default, ENV["RACK_ENV"])
 
 # Set up ActiveRecord and connect to DB
 require "active_record"
+require "erb"
 ActiveRecord::Base.record_timestamps = true
 ActiveRecord::Base.timestamped_migrations = false
 ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.include_root_in_json = false
-ActiveRecord::Base.establish_connection(YAML::load(File.open("config/database.yml")))
+ActiveRecord::Base.establish_connection(YAML.load(ERB.new(File.read('config/database.yml')).result)[ENV["RACK_ENV"]])
 
 require "foreigner"
 Foreigner.load
