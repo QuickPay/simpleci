a0ef3f3a35398ec28a5a3ea1f0d1d97b23efa5ec
Tonni Tølbøll Lund Aagesen <tonni@toelboell-lund.dk>
2012-12-31 16:17:25 +0100

    Switch to the more flexible database url for database settings

--- config/environment.rb
+++ config/environment.rb
@@ -11,12 +11,12 @@ require "bundler/setup"
 
 # Set up ActiveRecord and connect to DB
 require "active_record"
-require "erb"
+require "config/database" if File.exists?("config/database.rb")
 ActiveRecord::Base.record_timestamps = true
 ActiveRecord::Base.timestamped_migrations = false
 ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.include_root_in_json = false
-ActiveRecord::Base.establish_connection(YAML.load(ERB.new(File.read('config/database.yml')).result)[ENV["RACK_ENV"]])
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 
 require "foreigner"
 Foreigner.load
